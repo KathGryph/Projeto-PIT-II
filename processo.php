baf5780349821ae2562ee9e44982d5c34198e638
Kath <113138788+KathGryph@users.noreply.github.com>
2022-11-05 20:32:39 -0300

    Update processo.php
    Retirado informações sigilosas de banco de dados.

--- processo.php
+++ processo.php
@@ -13,11 +13,11 @@
 		$email = $_POST["email"];
 		$mensagem = $_POST["mensagem"];
 
-//lembrar de mudar username a password fora do padrão
-$host = "advlorem.mysql.dbaas.com.br";
-$dbname = "advlorem";
-$username = "advlorem";
-$password = "Advuni204512!";
+		//lembrar de mudar username a password fora do padrão
+		$host = "host aqui";
+		$dbname = "db aqui";
+		$username = "user aqui";
+		$password = "senha aqui";
 
 		//Pegar dados para a db e inicializar conexão
 		$conn = mysqli_connect(hostname: $host, 
@@ -57,15 +57,15 @@ $password = "Advuni204512!";
 			$mail->isSMTP();                                            //Send using SMTP
 			$mail->Host       = 'email-ssl.com.br';           //Set the SMTP server to send through
 			$mail->SMTPAuth   = true;                                   //Enable SMTP authentication
-			$mail->Username   = 'advlorem@advocacialorem.com.br';                     //SMTP username
-			$mail->Password   = 'Loremipsum123!';                               //SMTP password
+			$mail->Username   = 'email aqui';                     //SMTP username
+			$mail->Password   = 'senha aqui';                               //SMTP password
 			$mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;            //Enable implicit TLS encryption
 			$mail->Port       = 587;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
 
 			//Recipients
-			$mail->setFrom('advlorem@advocacialorem.com.br', 'Lorem');
-			$mail->addAddress('advlorem@advocacialorem.com.br', 'Advocacia');     //Add a recipient
-			$mail->addReplyTo('advlorem@advocacialorem.com.br', 'Information');
+			$mail->setFrom('email aqui', 'Lorem');
+			$mail->addAddress('email aqui', 'Advocacia');     //Add a recipient
+			$mail->addReplyTo('email aqui', 'Information');
 
 			//Content
 			$mail->isHTML(true);                                  //Set email format to HTML
